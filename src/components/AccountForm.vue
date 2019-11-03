f09a963c2166e742aec2f9b820b72a444d768b0e
Bill <bill.chandos@gmail.com>
2019-11-02 18:34:01 -0700

    account form features

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -3,18 +3,30 @@
         <p class="form_els">
             <label class="transaction_form_label" for="bank_select">Bank:</label>
             <select v-model="bank_select" id="bank_select">
-                <option v-for="bank in banks" v-bind:key="bank.id" v-bind:value="bank.id">
-                    {{ bank.name }}
+                <option v-for="bank_ in banks" v-bind:key="bank_.id" v-bind:value="bank_.id">
+                    {{ bank_.name }}
                 </option>
             </select>
+        </p>
+        <p class="form_els">
+            <label class="transaction_form_label" for="name">Name:</label>
+            <input name="name" v-model="bank.name">
         </p>
         <p class="form_els">
             <label class="transaction_form_label" for="filename_re">Filename Regex:</label>
             <input name="filename_re" v-model="bank_settings.filename_re">
         </p>
         <p class="form_els">
+            <label class="transaction_form_label" for="debit_positive">Debit is positive:</label>
+            <input type="checkbox" name="debit_positive" v-model="bank_settings.debit_positive">
+        </p>
+        <p class="form_els">
             <label class="transaction_form_label" for="date_format">Date Format:</label>
             <input name="date_format" v-model="bank_settings.date_format">
+        </p>
+        <p class="form_els">
+            <input :disabled="!bank_select" type="submit" value="Save" class="btn">
+            <input :disabled="!bank_select" type="button" value="Delete" class="btn" @click="deleteAccount">
         </p>
     </form>
 </template>
@@ -23,6 +35,7 @@ export default {
     data () {
         return {
             banks: [],
+            bank: {},
             bank_select: null,
             bank_settings: {}
         }
@@ -39,18 +52,43 @@ export default {
     },
     watch: {
         bank_select: function() {
+            this.bank = this.banks.find((e) => { return e.id == this.bank_select });
             this.bank_settings = this.banks.find((e) => { return e.id == this.bank_select }).settings;
         }
     },
     methods: {
         postData: async function() {
-            let url = 'http://127.0.0.1:8080/api/v0.1/accounts';
-            let form_data = new FormData();
+            let url = 'http://127.0.0.1:8080/api/v0.1/accounts/' + this.bank_select;
+            let form_data = {
+                'name': this.bank.name,
+                'filename_re': this.bank_settings.filename_re,
+                'debit_positive': this.bank_settings.debit_positive,
+                'date_format': this.bank_settings.date_format,
+            }
             const response = await fetch(url, {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                
-                body: form_data // body data type must match "Content-Type" header
+                method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(form_data) // body data type must match "Content-Type" header
                 });
+            if (response.status==201) {
+                // refresh our account
+                const json_account = await response.json();
+                this.bank = json_account.payload;
+            }
+        },
+        deleteAccount: async function() {
+            if (this.bank_select) {
+                let url = 'http://127.0.0.1:8080/api/v0.1/accounts/' + this.bank_select;
+                const response = await fetch(url, {
+                    method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+                    });
+                if (response.status == 200) {
+                    let json = await response.json();
+                    this.banks = this.banks.filter((e) => { return e.id != json.payload.id});
+                    this.bank = {};
+                    this.$emit('deleted');
+                }
+            }
         }
     }
 }
